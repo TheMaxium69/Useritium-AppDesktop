e4b7e7e8ec3546cc493079120c55d04bc3e171c5
Maxime Tournier <mxm.tournier@gmail.com>
2024-04-10 00:29:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, shell} = require('electron')
 const path = require('path')
 
 
@@ -20,9 +20,91 @@ app.whenReady().then(() => {
 
     // mainWindow.loadFile('content/test.html')
     // mainWindow.loadFile('content/index.html').then(err => console.log(err))
-    mainWindow.loadURL('https://useritium.fr').then(r => console.log(r))
+    mainWindow.loadURL('https://useritium.fr/connect.php').then(r => console.log(r))
 
+    mainWindow.webContents.on('page-title-updated', (event, title) => {
+        mainWindow.setTitle('Useritium App - 0.1.0');
+    });
+
+    let lastPage = 0;
+    mainWindow.webContents.on('did-navigate', (event, url) => {
+        console.log('URL actuelle:', url);
+        // Vous pouvez ajouter ici le code pour réagir aux changements d'URL
+        
+        if (url.startsWith("https://useritium.fr/panel.php")){
+
+            console.log("panel");
+
+            lastPage = 1;
+
+        } else if (url === "https://useritium.fr/connect.php"){
+
+            console.log("connection");
+
+            lastPage = 0;
+
+        } else {
+
+            if (lastPage === 0){
+                mainWindow.loadURL('https://useritium.fr/connect.php');
+            } else if (lastPage === 1){
+                mainWindow.loadURL('https://useritium.fr/panel.php');
+            }
+
+            shell.openExternal(url);
+        }
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.webContents.insertCSS(`
+            
+body {
+    overflow-y: auto;
+}
+
+body::-webkit-scrollbar {
+    width: 5px;
+    
+}
+
+body::-webkit-scrollbar-track {
+    background: #3d3d3d; // BackCouleur  
+}
+
+body::-webkit-scrollbar-thumb {
+    background: #1e1e1e;
+    border-radius: 4px;
+}
+
+body::-webkit-scrollbar-thumb:hover {
+    background: #000926;
+}
+
+        `);
+        mainWindow.webContents.executeJavaScript(`
+        
+        `);
+    });
 
     mainWindow.setMenuBarVisibility(false);
 
 })
+

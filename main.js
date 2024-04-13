a664a905b43ff4498d3489754bdacfcc13bd0f7d
Maxime Tournier <mxm.tournier@gmail.com>
2024-04-13 19:18:33 +0200

    Update

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const {app, BrowserWindow, shell} = require('electron')
+const {app, BrowserWindow, shell, session } = require('electron')
 const path = require('path')
 
 
 let mainWindow;
+let VersionApp = "0.2.0"
+let UrlBases = "http://127.0.0.1/";
+let UrlConnection = "http://127.0.0.1/Useritium-WebSite/connect.php"
+let UrlPanel = "http://127.0.0.1/Useritium-WebSite/panel.php"
 app.whenReady().then(() => {
+    // GESTION DE LA FENETRE
     mainWindow = new BrowserWindow({
         frame: true,
-        title: "Useritium App - 0.1.0",
+        title: "Useritium App - " + VersionApp,
         width: 1318,
         height: 710,
-        resizable: true,
+        resizable: false,
         icon: path.join(__dirname, "/assets/logo.png"),
         webPreferences: {
             webSecurity: true,
@@ -18,93 +23,44 @@ app.whenReady().then(() => {
         }
     })
 
-    // mainWindow.loadFile('content/test.html')
-    // mainWindow.loadFile('content/index.html').then(err => console.log(err))
-    mainWindow.loadURL('https://useritium.fr/connect.php').then(r => console.log(r))
+    // LANCEMENT DE L'APP
+    mainWindow.loadURL(UrlConnection).then(r => console.log("loadUrl", r))
+    mainWindow.setMenuBarVisibility(false);
 
+    // TITRE DE L'APP
     mainWindow.webContents.on('page-title-updated', (event, title) => {
-        mainWindow.setTitle('Useritium App - 0.1.0');
+        mainWindow.setTitle('Useritium App - ' + VersionApp);
     });
 
+    // GESTION DES URLS
     let lastPage = 0;
     mainWindow.webContents.on('did-navigate', (event, url) => {
         console.log('URL actuelle:', url);
-        // Vous pouvez ajouter ici le code pour réagir aux changements d'URL
-        
-        if (url.startsWith("https://useritium.fr/panel.php")){
 
+        if (url.startsWith(UrlPanel)){
             console.log("panel");
-
             lastPage = 1;
-
-        } else if (url === "https://useritium.fr/connect.php"){
-
+        } else if (url === UrlConnection){
             console.log("connection");
-
             lastPage = 0;
-
         } else {
 
             if (lastPage === 0){
-                mainWindow.loadURL('https://useritium.fr/connect.php');
+                mainWindow.loadURL(UrlConnection);
             } else if (lastPage === 1){
-                mainWindow.loadURL('https://useritium.fr/panel.php');
+                mainWindow.loadURL(UrlConnection);
             }
 
             shell.openExternal(url);
         }
+    });
 
-
+    const cookie = { url: UrlBases, name: 'DesktopApp', value: 'on' };
+    session.defaultSession.cookies.set(cookie, (error) => {
+        if (error) console.error(error);
     });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    mainWindow.webContents.on('did-finish-load', () => {
-        mainWindow.webContents.insertCSS(`
-            
-body {
-    overflow-y: auto;
-}
-
-body::-webkit-scrollbar {
-    width: 5px;
-    
-}
-
-body::-webkit-scrollbar-track {
-    background: #3d3d3d; // BackCouleur  
-}
-
-body::-webkit-scrollbar-thumb {
-    background: #1e1e1e;
-    border-radius: 4px;
-}
-
-body::-webkit-scrollbar-thumb:hover {
-    background: #000926;
-}
-
-        `);
-        mainWindow.webContents.executeJavaScript(`
-        
-        `);
-    });
-
-    mainWindow.setMenuBarVisibility(false);
 
 })
 
